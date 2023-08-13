805e3ce5c2905cd891449b766c1bbd5323b12a84
dangquangcute <quang0167799@gmail.com>
2023-08-13 23:15:27 +0000

    Aug 14, 2023, 6:15 AM

--- LearnVid/bla/main.js
+++ LearnVid/bla/main.js
@@ -5,12 +5,8 @@ function run(arr1,arr2)
 		var str = arr1.shift();
     if(arr2.length>0)
     {
-        arr2.forEach(element => {
-            if(element != str)
-            {
-                arr2.push(str)
-                break;
-            }
+        arr2.every(element => {
+            return element != str ? arr2.push(str) :null;
         });
     }
     else{
