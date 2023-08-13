f62334641493f1446be2de84907d7206439c2a1a
dangquangcute <quang0167799@gmail.com>
2023-08-13 14:06:41 +0000

    Aug 13, 2023, 9:06 PM

--- LearnVid/bla/main.js
+++ LearnVid/bla/main.js
@@ -1,6 +1,5 @@
-function run(arr1)
+function run(arr1,arr2)
 {
-	var arr2 =[];
   if(arr1.length > 0)
   {
 		var str = arr1.pop();
@@ -8,12 +7,12 @@ function run(arr1)
     delete arr1[length-1];
     console.log("arr1 : "+ arr1);
     console.log("arr2 : " + arr2);
-    return run(arr1);
+    return run(arr1,arr2);
   }
-  return arr2;
+  return;
   
 }
-
-var arr2 = run(['a','b','c','a','b']);
+var arrkk = [];
+run(['a','b','c','a','b'],arrkk);
 console.log(arr2)
 
